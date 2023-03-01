96d235ae9bca17a07da5228cdc711f2a3bc8119c
jayeshkumarcp <104484995+jayeshkumarcp@users.noreply.github.com>
2023-03-01 12:36:40 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "AWS_REGION" {    
+    default = "eu-west-2"
+}
+
+variable "aws_access_key" {
+  type        = string
+  description = "AWS access key"
+}
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS secret key"
+}
+variable "aws_region" {
+  type        = string
+  description = "AWS region"
+}
